265961d4e6fca6022eb557d2530c43db701c771d
oyindolapo <oyindolapokomolafe@yahoo.com>
2021-06-18 17:26:09 +0100

    Basic calculator functions

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # Scientific Python Calculator
 
-{Just a calculator for now}
+Scientific Python Calculator (scipy_calculator) is a library for doing mathematics computation. It works similar to a calculator.
 
-## Background
+:raised_hands: This is my first python package. It will be improved over time. Feel free to contribute to this beautiful work of art.
 
 ## Installation
+:warning: scipy_calculator is only tested with python 3. 
+
+*Installation via PYPI*
 ```
-	pip install scipy_calculator
+	$ pip install scipy_calculator
+```
+*Installation via Github*
+this is for the stable released version
+```
+	$ pip install git+""
 ```
 
-## Usage
-Average function resets the calculator memory before execution
+scipy_calculator uses multipledispatch dependency and pytest for automated test
 
 ## Sample Code
+#### Addition
+#### Subtraction
+#### Multiply
+#### Division
+#### Average
+#### Modulo
+#### Root
+#### Power
--- src/calculator.py
+++ src/calculator.py
@@ -1,13 +1,12 @@
 from typing import List
 from multipledispatch import dispatch
-import math
+import itertools
 
 
 class Calculator:
-    
+
     def __init__(self) -> None:
         self.__index = 0
-        self.__operands = []
         self.__operator = []
 
     @property
@@ -18,55 +17,217 @@ class Calculator:
         return self.__index
 
     @property
+    def operator(self):
+        return self.__operator
+
+    @property
     def reset(self):
         """
             Reset memory to 0
         """
         self.__index = 0
-        self.__operands = []
         self.__operator = []
 
     def average(self, item: list[float]) -> float:
+        self.reset
         self.__index = sum(item)/len(item)
+        self.__operator.append("average: " + ','.join(str(x) for x in item))
         return self.__index
 
-    @dispatch(list)
-    def sum(self, item: list[float]) -> float:
-        self.__index += sum(item)
-        return self.__index
+    def sum(self, *args):
+        if(len(args) > 1):
+            if isinstance(args, tuple):
+                for i in args:
+                    self.__index += i
+        else:
+            if isinstance(*args, int):
+                for ele in args:
+                    self.__index += ele
+            else:
+                flat_list = list(itertools.chain(*args))
+                for x in flat_list:
+                    self.__index += x
 
-    @dispatch(int, int)
-    def sum(self, a: int, b: int = 0) -> int:
-        self.__index = self.__index + a + b
+        self.__operator.append("sum: " + ',' + str(args))
         return self.__index
 
     @dispatch(int)
-    def sum(self, a: int) -> int:
-        self.__index += a
+    def subtract(self, a: int) -> int:
+        self.__index -= a
+        self.__operator.append("subtract: " + str(a))
+        return round(self.__index, 0)
+
+    @dispatch(float)
+    def subtract(self, a: float) -> float:
+        self.__index -= a
+        self.__operator.append("subtract: " + str(a))
+        return round(self.__index, 2)
+
+    @dispatch(float, float)
+    def subtract(self, a: float, b: float) -> float:
+        if self.__index == 0:
+            self.__index = a - b
+        else:
+            self.__index = self.__index - a - b
+        self.__operator.append("subtract: " + str(a) + "," + str(b))
+        return self.__index
+
+    @dispatch(float, int)
+    def subtract(self, a: float, b: int = None) -> float:
+        if self.__index == 0:
+            self.__index = a - b
+        else:
+            self.__index = self.__index - a - b
+        self.__operator.append("subtract: " + str(a) + "," + str(b))
         return self.__index
 
     @dispatch(object, object)
-    def sum(self, a: float = 0, b: float = 0) -> object:
-        self.__index += a + b
+    def subtract(self, a, b):
+        if self.__index == 0:
+            self.__index = a - b
+        else:
+            self.__index = self.__index - a - b
+        self.__operator.append("subtract: " + str(a) + "," + str(b))
         return self.__index
 
-    def subtract(self, a: float, b: float = None):
-        pass
+    @dispatch(int, int)
+    def power(self, base: int, super: int) -> int:
+        pow = 1
+        for x in range(super):
+            pow *= base
+
+        self.__index = pow
+        return self.__index
 
 
+    @dispatch(float, int)
+    def power(self, base: float, super: int) -> float:
+        pow = 1
+        for x in range(super):
+            pow *= base
+        self.__index = pow
+        return self.__index
 
+    def multiply(self, *args):
+        mult = 1
+        if(len(args) > 1):
+            if isinstance(args, tuple):
+                for i in args:
+                    mult *= i
+        else:
 
+            if isinstance(*args, int):
+                for ele in args:
+                    mult *= ele
+            else:
+                flat_list = list(itertools.chain(*args))
+                for x in flat_list:
+                    mult *= x
 
-grade = [10, 12.90, 14, 5, 7]
-cal = Calculator()
-val = cal.sum(12)
-print(f"sum is {val}")
+        self.__index = mult
+        self.__operator.append("multiply: " + ',' + str(args))
+        return self.__index
 
+    @dispatch(int, int)
+    def divide(self, numerator: int, denominator: int) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
 
-cal.reset
+        else:
+            self.__index = numerator/denominator
+            return self.__index
 
-print(cal.sum(grade))
-print(cal.sum(1))
-print(cal.average(grade))
-print(cal.sum(4))
-print(cal.memory_val)
+    @dispatch(float, int)
+    def divide(self, numerator: float, denominator: int) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
+
+        else:
+            self.__index = numerator/denominator
+            return self.__index
+
+    @dispatch(int, float)
+    def divide(self, numerator: int, denominator: float) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
+
+        else:
+            self.__index = numerator/denominator
+            return self.__index
+
+    @dispatch(float, float)
+    def divide(self, numerator: float, denominator: float):
+        if denominator == 0:
+            return 'err'
+
+        else:
+            self.__index = numerator/denominator
+            return self.__index
+
+    @dispatch(int)
+    def divide(self, denominator: int) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
+        else:
+            self.__index /= denominator
+            return self.__index
+
+    @dispatch(float)
+    def divide(self, denominator: float) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
+        else:
+            self.__index /= denominator
+            return self.__index
+
+    @dispatch(int, int)
+    def modulo(self, numerator: int, denominator: int) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
+
+        else:
+            self.__index = numerator % denominator
+            return self.__index
+
+    @dispatch(float, int)
+    def modulo(self, numerator: float, denominator: int) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
+
+        else:
+            self.__index = numerator % denominator
+            return self.__index
+
+    @dispatch(int, float)
+    def modulo(self, numerator: int, denominator: float) -> float:
+        if denominator == 0:
+            self.reset
+            return 'err'
+
+        else:
+            self.__index = numerator % denominator
+            return self.__index
+
+    @dispatch(float, float)
+    def modulo(self, numerator: float, denominator: float):
+        if denominator == 0:
+            return 'err'
+
+        else:
+            self.__index = numerator % denominator
+            return self.__index
+
+    def root(self, base, root):
+        if base > 0:
+            rst = base**(1./root)
+        else:
+            rst = -(base**(1./root))
+        self.__index = rst
+        return self.__index
