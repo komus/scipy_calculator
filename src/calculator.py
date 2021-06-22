f29d33b17d5ac3c9eee86dcc44b604bda950ee8c
oyindolapo <oyindolapokomolafe@yahoo.com>
2021-06-22 16:35:31 +0100

    included type validation

--- src/calculator.py
+++ src/calculator.py
@@ -26,6 +26,11 @@ class Calculator:
         '''
         return self.__index
 
+    @staticmethod
+    def __validate_input(number: Union[int, float]):
+        if not isinstance (number, int) and not isinstance(number, float):
+            raise TypeError("accepted types is int or float")
+
     def reset(self):
         """
             Reset memory to 0
@@ -34,18 +39,22 @@ class Calculator:
         self.__operator = []
 
     def add(self, num: Union[int, float]):
+        self.__validate_input(num)
         self.__index += num
         return self.__index
 
     def subtract(self, num: Union[int, float]):
+        self.__validate_input(num)
         self.__index -= num
         return self.__index
 
     def multiply(self, num: Union[int, float]):
+        self.__validate_input(num)
         self.__index *= num
         return self.__index
 
     def divide(self, num: Union[int, float]):
+        self.__validate_input(num)
         try:
             self.__index /= num
             return self.__index
@@ -53,16 +62,19 @@ class Calculator:
             print(f"number cannot be zero => {err}")
 
     def modulo(self, num: Union[int, float]):
+        self.__validate_input(num)
         try:
             self.__index %= num
             return self.__index
         except ZeroDivisionError as err:
             print(f"number cannot be zero => {err}")
 
-    def sqrt(self, num: Union[int, float]):
-        try:
-            self.__index = num**(1./2)
-            return self.__index
-        except ZeroDivisionError as err:
-            print(f"number cannot be zero => {err}")
+    def root(self, num: Union[int, float]):
+        self.__validate_input(num)
+        if self.__index <= 0:
+            raise ValueError(f"memory value must be greater than zero")
+        if num <= 0:
+            raise ValueError("root must be greater than 0")
 
+        self.__index = self.__index**(1./num)
+        return self.__index
