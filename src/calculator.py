0e08c798aabff090b068d245accefe9c559a925f
oyindolapo <oyindolapokomolafe@yahoo.com>
2021-06-22 09:06:09 +0100

    Updated functions

--- src/calculator.py
+++ src/calculator.py
@@ -1,6 +1,4 @@
-from typing import List
-from multipledispatch import dispatch
-import itertools
+from typing import Union
 
 
 class Calculator:
@@ -10,13 +8,12 @@ class Calculator:
         This class handles basic operations of a calculator.
     
         Mathematics Operations:
-        - Power
         - Addition
         - Subtraction
         - Multiplication
         - Division
         - Modulo
-        - Root
+        - Sqrt
     """
     def __init__(self) -> None:
         self.__index = 0
@@ -29,11 +26,6 @@ class Calculator:
         '''
         return self.__index
 
-    @property
-    def operator(self):
-        return self.__operator
-
-    @property
     def reset(self):
         """
             Reset memory to 0
@@ -41,201 +33,36 @@ class Calculator:
         self.__index = 0
         self.__operator = []
 
-
-    def sum(self, *args):
-        if(len(args) > 1):
-            if isinstance(args, tuple):
-                for i in args:
-                    self.__index += i
-        else:
-            if isinstance(*args, int):
-                for ele in args:
-                    self.__index += ele
-            else:
-                flat_list = list(itertools.chain(*args))
-                for x in flat_list:
-                    self.__index += x
-
-        self.__operator.append("sum: " + ',' + str(args))
+    def add(self, num: Union[int, float]):
+        self.__index += num
         return self.__index
 
-    @dispatch(int)
-    def subtract(self, a: int) -> int:
-        self.__index -= a
-        self.__operator.append("subtract: " + str(a))
-        return round(self.__index, 0)
-
-    @dispatch(float)
-    def subtract(self, a: float) -> float:
-        self.__index -= a
-        self.__operator.append("subtract: " + str(a))
-        return round(self.__index, 2)
-
-    @dispatch(float, float)
-    def subtract(self, a: float, b: float) -> float:
-        if self.__index == 0:
-            self.__index = a - b
-        else:
-            self.__index = self.__index - a - b
-        self.__operator.append("subtract: " + str(a) + "," + str(b))
+    def subtract(self, num: Union[int, float]):
+        self.__index -= num
         return self.__index
 
-    @dispatch(float, int)
-    def subtract(self, a: float, b: int = None) -> float:
-        if self.__index == 0:
-            self.__index = a - b
-        else:
-            self.__index = self.__index - a - b
-        self.__operator.append("subtract: " + str(a) + "," + str(b))
+    def multiply(self, num: Union[int, float]):
+        self.__index *= num
         return self.__index
 
-    @dispatch(object, object)
-    def subtract(self, a, b):
-        if self.__index == 0:
-            self.__index = a - b
-        else:
-            self.__index = self.__index - a - b
-        self.__operator.append("subtract: " + str(a) + "," + str(b))
-        return self.__index
-
-    @dispatch(int, int)
-    def power(self, base: int, super: int) -> int:
-        pow = 1
-        for x in range(super):
-            pow *= base
-
-        self.__index = pow
-        return self.__index
-
-
-    @dispatch(float, int)
-    def power(self, base: float, super: int) -> float:
-        pow = 1
-        for x in range(super):
-            pow *= base
-        self.__index = pow
-        return self.__index
-
-    def multiply(self, *args):
-        mult = 1
-        if(len(args) > 1):
-            if isinstance(args, tuple):
-                for i in args:
-                    mult *= i
-        else:
-
-            if isinstance(*args, int):
-                for ele in args:
-                    mult *= ele
-            else:
-                flat_list = list(itertools.chain(*args))
-                for x in flat_list:
-                    mult *= x
-
-        self.__index = mult
-        self.__operator.append("multiply: " + ',' + str(args))
-        return self.__index
-
-    @dispatch(int, int)
-    def divide(self, numerator: int, denominator: int) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-
-        else:
-            self.__index = numerator/denominator
+    def divide(self, num: Union[int, float]):
+        try:
+            self.__index /= num
             return self.__index
+        except ZeroDivisionError as err:
+            print(f"number cannot be zero => {err}")
 
-    @dispatch(float, int)
-    def divide(self, numerator: float, denominator: int) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-
-        else:
-            self.__index = numerator/denominator
+    def modulo(self, num: Union[int, float]):
+        try:
+            self.__index %= num
             return self.__index
+        except ZeroDivisionError as err:
+            print(f"number cannot be zero => {err}")
 
-    @dispatch(int, float)
-    def divide(self, numerator: int, denominator: float) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-
-        else:
-            self.__index = numerator/denominator
+    def sqrt(self, num: Union[int, float]):
+        try:
+            self.__index = num**(1./2)
             return self.__index
+        except ZeroDivisionError as err:
+            print(f"number cannot be zero => {err}")
 
-    @dispatch(float, float)
-    def divide(self, numerator: float, denominator: float):
-        if denominator == 0:
-            return 'err'
-
-        else:
-            self.__index = numerator/denominator
-            return self.__index
-
-    @dispatch(int)
-    def divide(self, denominator: int) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-        else:
-            self.__index /= denominator
-            return self.__index
-
-    @dispatch(float)
-    def divide(self, denominator: float) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-        else:
-            self.__index /= denominator
-            return self.__index
-
-    @dispatch(int, int)
-    def modulo(self, numerator: int, denominator: int) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-
-        else:
-            self.__index = numerator % denominator
-            return self.__index
-
-    @dispatch(float, int)
-    def modulo(self, numerator: float, denominator: int) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-
-        else:
-            self.__index = numerator % denominator
-            return self.__index
-
-    @dispatch(int, float)
-    def modulo(self, numerator: int, denominator: float) -> float:
-        if denominator == 0:
-            self.reset
-            return 'err'
-
-        else:
-            self.__index = numerator % denominator
-            return self.__index
-
-    @dispatch(float, float)
-    def modulo(self, numerator: float, denominator: float):
-        if denominator == 0:
-            return 'err'
-
-        else:
-            self.__index = numerator % denominator
-            return self.__index
-
-    def root(self, base, root):
-        if base > 0:
-            rst = base**(1./root)
-        else:
-            rst = -(base**(1./root))
-        self.__index = rst
-        return self.__index
--- test/test_basic_call.py
+++ test/test_basic_call.py
@@ -1,85 +1,40 @@
+import sys
+import pytest
+sys.path.append("../src")
 from calculator import Calculator
 
+cal = Calculator()
 
 def test_add():
-    assert Calculator().sum(10, 2) == 12
-    assert Calculator().sum(2) == 2
-
-    cal = Calculator()
-    assert cal.sum(2) == 2
-    assert cal.sum(2, 4) == 8
-    assert cal.sum([10, 12.90, 14, 5, 7]) == 56.9
-
-    cal.reset
-
-    assert cal.sum(3, 2) == 5
+    assert cal.add(10) == 10
 
 
 def test_subtract():
-    assert Calculator().subtract(2) == -2
-    assert Calculator().subtract(10, 6) == 4
-
-    cal = Calculator()
-    assert cal.subtract(10, 4) == 6
-    assert cal.subtract(2) == 4
-    assert cal.subtract(3, 1) == 0
-
-    cal.reset
-    assert cal.subtract(2, 7) == -5
-
-
-def test_power():
-    assert Calculator().power(5, 3) == 125
-    assert Calculator().power(3.5, 3) == 42.875
-
-    cal = Calculator()
-    assert cal.power(2, 5) == 32
-    assert cal.sum(3) == 35
-
-    cal.reset
-
-    assert cal.power(2, 2) == 4
-    assert cal.subtract(3) == 1
-
-
-def test_multiply():
-    val = [2, 4, 5, 2, 1]
-    assert Calculator().multiply(val) == 80
-    assert Calculator().multiply(10, 4) == 40
-
-    cal = Calculator()
-    assert cal.multiply(12, 2) == 24
-    assert cal.subtract(4) == 20
-
-    assert cal.multiply(1000) == 1000
-
+    assert cal.subtract(5) == 5
 
 def test_divide():
-    assert Calculator().divide(10) == 0
-    assert Calculator().divide(10, 2) == 5
+    assert cal.divide(2) == 2.5
 
-    cal = Calculator()
-    assert cal.divide(100, 5) == 20
-    assert cal.sum(10) == 30
-    assert cal.divide(2) == 15
-    assert cal.divide(0) == 'err'
-    assert cal.divide(10.78, 0) == 'err'
-    assert cal.divide(0, 78) == 0
+def test_memory_val():
+    assert cal.memory_val == 2.5
 
+def test_multiply():
+    assert cal.multiply(0) == 0
+
+def test_zero_division():
+    assert cal.subtract(20) == -20
+    assert cal.add(-10) == -30
+    assert cal.divide(0) == None
+    assert cal.memory_val == -30
 
 def test_modulo():
-    assert Calculator().modulo(10, 2) == 0
-    assert Calculator().modulo(15, 4) == 3
+    assert cal.modulo(3) == 0
+    assert cal.memory_val == 0
 
-    cal = Calculator()
-    assert cal.modulo(10, 0) == 'err'
-    assert cal.modulo(14.67, 3) == 2.67
+def test_sqrt():
+    assert cal.sqrt(16) == 4
+    assert cal.memory_val == 4
 
-
-
-def test_root():
-    assert Calculator().root(27, 3) == 3
-    assert Calculator().root(32, 5) == 2
-
-    cal = Calculator()
-    cal.root(-8, 2) == -2
+def test_reset():
+    cal.reset()
+    assert cal.memory_val == 0
